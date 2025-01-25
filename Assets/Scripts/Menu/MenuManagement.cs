423a78e411fba5d20bb9162391d59336c13b0cab
Valentin D <118002443+Yukigana@users.noreply.github.com>
2025-01-25 14:00:23 +0100

    upd: player cone vision (#7)

--- Assets/Scripts/Menu/MenuManagement.cs
+++ Assets/Scripts/Menu/MenuManagement.cs
@@ -2,33 +2,33 @@ using UnityEngine;
 
 public class MenuManagement : MonoBehaviour
 {
-    [Header("Menu")]
-    [SerializeField]
-    private GameObject mainMenu;
-    [SerializeField]
-    private GameObject optionsMenu;
-    
+	[Header("Menu")]
+	[SerializeField]
+	private GameObject mainMenu;
+	[SerializeField]
+	private GameObject optionsMenu;
 
-    public void Start()
-    {
-        mainMenu.SetActive(true);
-        optionsMenu.SetActive(false);
-    }
 
-    public void OpenOptionsMenu()
-    {
-        mainMenu.SetActive(false);
-        optionsMenu.SetActive(true);
-    }
+	public void Start()
+	{
+		mainMenu.SetActive(true);
+		optionsMenu.SetActive(false);
+	}
 
-    public void CloseOptionsMenu()
-    {
-        mainMenu.SetActive(true);
-        optionsMenu.SetActive(false);
-    }
+	public void OpenOptionsMenu()
+	{
+		mainMenu.SetActive(false);
+		optionsMenu.SetActive(true);
+	}
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+	public void CloseOptionsMenu()
+	{
+		mainMenu.SetActive(true);
+		optionsMenu.SetActive(false);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
 }
