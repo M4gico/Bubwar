e32715883b9ca4c41d79a8e897d5628825b8a4b1
M4gico <98741576+M4gico@users.noreply.github.com>
2025-01-25 19:20:56 +0100

    Sprite player et jauge de tir + autres (#9)
    * Player movement and rotation
    
    * Add camera
    
    * Start of the weapon
    
    * Weapon system
    
    * Buuble animation
    
    * Child follow player smoothly
    
    * Child automatic weapon
    
    * Sprite + orientation + gudge

---
+++ Assets/Scripts/Child/ChildWeapon.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class ChildWeapon : MonoBehaviour
+{
+    [SerializeField] private float cooldownToShot;
+    [SerializeField] private GameObject bubbleBullet;
+    [SerializeField] private Transform forwardWeapon;
+    [SerializeField] private float forceOfTheShot;
+
+    private float gunHeat;
+    private ChildFollow childFollow;
+    private Transform childTransform;
+    private Vector3 playerPosition;
+
+    private void Awake()
+    {
+        childFollow = GetComponentInParent<ChildFollow>();
+        childTransform = GetComponentInParent<Transform>();
+    }
+
+    private void Update()
+    {
+        //Get the difference between the player and the child
+        //differenceToTarget = childFollow.differenceToTarget.magnitude;
+        playerPosition = childFollow.playerTransform.position;
+
+        gunHeat += Time.deltaTime;
+        if(gunHeat > cooldownToShot)
+        {
+            GameObject bubbleBulletGO = Instantiate(bubbleBullet, forwardWeapon.position, childTransform.rotation);
+            gunHeat = 0;
+            Rigidbody2D bubbleBulletrb = bubbleBulletGO.GetComponent<Rigidbody2D>();
+
+            //Get the orientation of the weapon compared to the mouse
+            transform.right = new Vector2(playerPosition.x,playerPosition.y) - new Vector2(transform.position.x, transform.position.y);
+            bubbleBulletrb.AddForce(transform.right * forceOfTheShot);
+        }
+    }
+}
