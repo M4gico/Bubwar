3b624f5b3a36ba445dfed73eff75634d5b62e641
Yukigana <valentin.debonnefoy@gmail.com>
2025-01-25 12:22:12 +0100

    add: armory UI with fonctionnal buttons

---
+++ Assets/PlayerMenuManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerMenuManager : MonoBehaviour
+{
+  [Header("Menu")]
+  [SerializeField]
+  private GameObject armoryMenu;
+
+  [SerializeField]
+  private GameObject optionsMenu;
+
+  public void Start()
+  {
+    armoryMenu.SetActive(false);
+    optionsMenu.SetActive(false);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void OpenArmoryMenu()
+  {
+    armoryMenu.SetActive(true);
+    optionsMenu.SetActive(false);
+  }
+
+  public void CloseMenu()
+  {
+    armoryMenu.SetActive(false);
+    optionsMenu.SetActive(false);
+  }
+
+  public void OpenOptionsMenu()
+  {
+    armoryMenu.SetActive(false);
+    optionsMenu.SetActive(true);
+  }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,67 +3,67 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float smoothFactor;
+  [SerializeField] private float moveSpeed;
+  [SerializeField] private float smoothFactor;
 
-    private Camera cam;
+  private Camera cam;
 
-    private Rigidbody2D rb;
+  private Rigidbody2D rb;
 
-    private InputAction moveAction;
-    private InputAction lookAction;
+  private InputAction moveAction;
+  private InputAction lookAction;
 
-    //Movement and look of the player
-    private Vector2 moveValue;
-    private Vector2 lookValue;
-    private Vector3 vectorZero = Vector3.zero;
-    private Vector3 mousPos;
-    private float angleRadMouse;
-    private float angleDegMouse;
+  //Movement and look of the player
+  private Vector2 moveValue;
+  private Vector2 lookValue;
+  private Vector3 vectorZero = Vector3.zero;
+  private Vector3 mousPos;
+  private float angleRadMouse;
+  private float angleDegMouse;
 
-    private static PlayerMovement instance;
+  private static PlayerMovement instance;
 
-    private void Awake()
+  private void Awake()
+  {
+    if (instance != null)
     {
-        if(instance != null)
-        {
-            if (instance != null)
-            {
-                Debug.LogWarning("There is more than one instance of PlayerMovement");
-                return;
-            }
+      if (instance != null)
+      {
+        Debug.LogWarning("There is more than one instance of PlayerMovement");
+        return;
+      }
 
-            instance = this;
-        }
-
-        cam = Camera.main;
-        rb = GetComponent<Rigidbody2D>();
+      instance = this;
     }
 
-    private void Start()
-    {
-        moveAction = InputSystem.actions.FindAction("Move");
-        lookAction = InputSystem.actions.FindAction("CursorPos");
-    }
+    cam = Camera.main;
+    rb = GetComponent<Rigidbody2D>();
+  }
 
-    private void FixedUpdate()
-    {
-        //Get the value of inputs
-        moveValue = moveAction.ReadValue<Vector2>();
-        lookValue = lookAction.ReadValue<Vector2>();
+  private void Start()
+  {
+    moveAction = InputSystem.actions.FindAction("Move");
+    lookAction = InputSystem.actions.FindAction("CursorPos");
+  }
 
-        //Find the position between the cursor and the character
-        mousPos = (Vector2)cam.ScreenToWorldPoint(lookValue);
-        angleRadMouse = Mathf.Atan2(mousPos.y - transform.position.y, mousPos.x - transform.position.x);
-        angleDegMouse = (180 / Mathf.PI) * angleRadMouse - 90;
+  private void FixedUpdate()
+  {
+    //Get the value of inputs
+    moveValue = moveAction.ReadValue<Vector2>();
+    lookValue = lookAction.ReadValue<Vector2>();
 
-        transform.rotation = Quaternion.Euler(0f, 0f, angleDegMouse);
-        //Draw a line between the mouse and the player by a white line and actualize it every tick
-        Debug.DrawLine(transform.position, mousPos, Color.white, Time.deltaTime);
+    //Find the position between the cursor and the character
+    mousPos = (Vector2)cam.ScreenToWorldPoint(lookValue);
+    angleRadMouse = Mathf.Atan2(mousPos.y - transform.position.y, mousPos.x - transform.position.x);
+    angleDegMouse = (180 / Mathf.PI) * angleRadMouse - 90;
 
-        //Move the player
-        Vector3 vel = new Vector2(moveValue.x * moveSpeed, moveValue.y*moveSpeed);
-        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, vel, ref vectorZero, smoothFactor);
-    }
+    transform.rotation = Quaternion.Euler(0f, 0f, angleDegMouse);
+    //Draw a line between the mouse and the player by a white line and actualize it every tick
+    Debug.DrawLine(transform.position, mousPos, Color.white, Time.deltaTime);
+
+    //Move the player
+    Vector3 vel = new Vector2(moveValue.x * moveSpeed, moveValue.y * moveSpeed);
+    rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, vel, ref vectorZero, smoothFactor);
+  }
 
 }
