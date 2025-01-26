e5609fe5db6079533585b4f9d8a156f21491b80b
Yukigana <valentin.debonnefoy@gmail.com>
2025-01-26 16:30:00 +0100

    fix&upg: gameplay loop

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,10 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] private float initialHealth = 3;
     [SerializeField] private float health;
     [SerializeField] private float maxHealth;
-    [SerializeField] [Range(0.05f, 1f)] private float invincibilityFlashDelay;
-    [SerializeField] [Range(0.1f, 3f)] private float invincibilityTimeAfterHit;
+    [SerializeField][Range(0.05f, 1f)] private float invincibilityFlashDelay;
+    [SerializeField][Range(0.1f, 3f)] private float invincibilityTimeAfterHit;
 
     [Header("UI part")]
     [SerializeField] private List<Animator> hearthAnimator;
@@ -39,7 +40,7 @@ public class PlayerHealth : MonoBehaviour
             }
             else
             {
-                GetComponent < PlayerSound>().PlayTakeDamageSound();
+                GetComponent<PlayerSound>().PlayTakeDamageSound();
             }
             isInvincible = true;
             Debug.Log("Player health " + health);
@@ -52,11 +53,15 @@ public class PlayerHealth : MonoBehaviour
 
     private IEnumerator PlayerDead()
     {
-        rb.bodyType = RigidbodyType2D.Kinematic;
-        rb.linearVelocity = Vector3.zero;
-        playerMovement.enabled = false;
+        // rb.bodyType = RigidbodyType2D.Kinematic;
+        // rb.linearVelocity = Vector3.zero;
         yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("MainMenuScene");
+
+        GameManager.instance.UnSetupPlayer();
+        ResetPlayerHealth();
+        ActualiseHearth();
+
     }
 
     public void AddLife(float life)
@@ -100,5 +105,14 @@ public class PlayerHealth : MonoBehaviour
     {
         yield return new WaitForSeconds(invincibilityTimeAfterHit);
         isInvincible = false;
+    }
+
+    public void ResetPlayerHealth()
+    {
+        maxHealth = initialHealth;
+        health = maxHealth;
+        isInvincible = false;
+        ActualiseHearth();
+        graphics.color = new Color(1f, 1f, 1f, 1f);
     }
 }
