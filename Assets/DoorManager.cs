90c38fa40c3fdc1dff31289e59a08789fef72288
Yukigana <valentin.debonnefoy@gmail.com>
2025-01-26 14:22:52 +0100

    add: intro main menu

--- Assets/DoorManager.cs
+++ Assets/DoorManager.cs
@@ -7,12 +7,17 @@ public class DoorManager : MonoBehaviour
     [SerializeField]
     private Sprite doorClosed;
 
+    [SerializeField]
+    private bool isDoorOpen;
+
     private void Start()
     {
         gameObject.GetComponent<SpriteRenderer>().sprite = doorClosed;
         gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
         gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0, 0);
         gameObject.GetComponent<BoxCollider2D>().size = new Vector2(1.75f, 1.85f);
+
+        isDoorOpen = false;
     }
 
 
@@ -22,11 +27,13 @@ public class DoorManager : MonoBehaviour
         gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
         gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0, 0.9f);
         gameObject.GetComponent<BoxCollider2D>().size = new Vector2(1.75f, 0.7f);
+
+        isDoorOpen = true;
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && isDoorOpen)
         {
             RoomManager.instance.LoadNextRoom();
         }
